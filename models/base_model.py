6ba7bdecf184a423d8baee46bb00019e1ce77eff
brianmaleek <maleekmalik@gmail.com>
2023-09-20 01:18:05 +0300

    pycodestyling 1

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,8 @@ class BaseModel:
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value,
-                                                         "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
                 elif key != '__class__':
                     setattr(self, key, value)
 
