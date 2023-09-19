fc28b1b3c09ad78d7e5edd93453ff82654b43f8c
washucode <estherw8525@gmail.com>
2023-09-20 01:28:13 +0300

    db storage - state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base if getenv('HBNB_TYPE_STORAGE') == 'db' else object):
     """ State class """
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                          cascade="delete")
+    else:
+        name = ""
+
+    @property
+    def cities(self):
+        """ getter for cities """
+        from models import storage
+        from models.city import City
+        cities = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                cities.append(city)
+        return cities
