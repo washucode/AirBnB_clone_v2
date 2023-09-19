98998d1cdab0e512e08591d5970ec6d77d2d7393
washucode <estherw8525@gmail.com>
2023-09-20 01:26:13 +0300

    configure init for sql alchemy

---
+++ models/engine/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+storage.reload()
