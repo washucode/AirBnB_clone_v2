762bb3e8d7aeeccf2635bba0cf2f98e239db8702
washucode <estherw8525@gmail.com>
2023-10-09 02:56:31 +0300

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/env bash
 # Script sets up your web servers for the deployment of web_static
 
@@ -61,4 +62,27 @@ echo "$nginx_config" | sudo tee /etc/nginx/sites-available/default > /dev/null
 sudo nginx -t
 
 # Restart Nginx to apply the changes
+=======
+#!/usr/bin/env/bash
+# Script that sets up your web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sudo sed -i '42i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart nginx
+>>>>>>> d96a6a9 (task 0)
 sudo service nginx restart
