95496571f47362f7bf70b8bc3c88dc78826e2546
washucode <estherw8525@gmail.com>
2023-10-09 03:50:11 +0300

    task 1 pycodestyle correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-    Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack.
+    Fabric script that generates a .tgz archive from the contents of the
+    web_static folder of your AirBnB Clone repo, using the function do_pack.
 """
 
-from  fabric.api import local
+from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """Function to compress files"""
@@ -15,5 +17,5 @@ def do_pack():
     try:
         local("tar -cvzf {} web_static".format(file))
         return file
-    except:
+    except Exception:
         return None
